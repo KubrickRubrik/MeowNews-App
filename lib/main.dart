af2803d1d0b84bae8e981ca21865837003fd0a3a
Maksym Tyunin <41119975+KubrickRubrik@users.noreply.github.com>
2023-08-18 14:03:42 +0300

    Fix main

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:news_test/presentation/locator/locator.dart';
 import 'package:news_test/presentation/manager/pages/news/provider.dart';
 import 'package:news_test/presentation/ui/pages/a_start/start.dart';
-import 'package:device_preview/device_preview.dart';
+// import 'package:device_preview/device_preview.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   ServicesLocator.setup();
   await locator<NewsProvider>().getInitNews();
-  // runApp(const StartPage());
-  runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => const StartPage(),
-    ),
-  );
+  runApp(const StartPage());
+  // runApp(
+  //   DevicePreview(
+  //     enabled: true,
+  //     builder: (context) => const StartPage(),
+  //   ),
+  // );
 }
